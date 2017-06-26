430408ccd17d0d0927e4c7285bfb081619811468
Ziyun Ma <zma@macbook-air-5.local>
2017-06-26 10:00:36 -0500

    Initial Commit

---
+++ Divvy Bike AppTests/Divvy_Bike_AppTests.swift
@@ -0,0 +1,36 @@
+//
+//  Divvy_Bike_AppTests.swift
+//  Divvy Bike AppTests
+//
+//  Created by Ziyun Ma on 6/26/17.
+//  Copyright © 2017 Ziyun Ma. All rights reserved.
+//
+
+import XCTest
+@testable import Divvy_Bike_App
+
+class Divvy_Bike_AppTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
